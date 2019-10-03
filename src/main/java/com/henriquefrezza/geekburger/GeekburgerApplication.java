eff51702447b08baba671eefce9cd2e46d15dcd4
henriquefrezza <henriquesantosfrezza@gmail.com>
2019-10-03 16:21:15 -0300

    primeira liberação

---
+++ src/main/java/com/henriquefrezza/geekburger/GeekburgerApplication.java
@@ -0,0 +1,13 @@
+package com.henriquefrezza.geekburger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekburgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekburgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henriquefrezza/geekburger/GeekburgerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.henriquefrezza.geekburger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GeekburgerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
